46b83cedfd6166289197b87a249228aeccb6e38f
wwog <871782513@qq.com>
2023-07-18 01:32:46 +0800

    -

--- packages/react-components/src/helper/cssVarariable.ts
+++ packages/react-components/src/helper/cssVarariable.ts
@@ -1,24 +1,14 @@
 import { CSSLayer, CSSRuleBlock } from '@wwog/css'
 
-export interface CSSVariableOptions {
-  /**
-   * @description 将某个时间段内的更新合并为一次更新
-   * @description_en Merge updates within a certain period of time into one update
-   * @default 15
-   */
-  mergeUpdate?: number
-}
-
 export class CSSVariable {
   public styleElement
 
   protected cssLayer: CSSLayer
   protected ruleBlock: CSSRuleBlock
-  protected updateTime: number
-  protected updateTimer: any
 
-  constructor(options: CSSVariableOptions = {}) {
-    const { mergeUpdate = 15 } = options
+  private req = false
+
+  constructor() {
     this.cssLayer = new CSSLayer({
       name: 'dynamic-css-variable',
     })
@@ -26,8 +16,7 @@ export class CSSVariable {
     this.cssLayer.addRuleBlock(this.ruleBlock)
     this.styleElement = document.createElement('style')
     this.styleElement.setAttribute('type', 'text/css')
-    this.styleElement.setAttribute('id','dynamic-css-variable')
-    this.updateTime = mergeUpdate
+    this.styleElement.setAttribute('id', 'dynamic-css-variable')
     document.head.appendChild(this.styleElement)
   }
 
@@ -48,17 +37,28 @@ export class CSSVariable {
   getVar(key: string) {
     return this.ruleBlock.rules[key]
   }
-
+  /**
+   * @example
+   * useLayoutEffect(() => {
+   *  cssVariable.addVar({ '--primary-color': primaryColor })
+   *  cssVariable.update()
+   * }, [primaryColor])
+   * @description 样式更新提交到页面。
+   * 如果位于`useEffect`调用,可能会遇到丢失更新,因为它不阻塞浏览器绘制。
+   * 优先考虑单独的`useLayoutEffect`中使用它,内部通过`requestAnimationFrame`在单帧内的多次调用会合并在重绘前与react渲染组件完成后提交更新于同一帧执行。
+   * @description_en Style update submitted to the page.
+   * If it is called in `useEffect`, you may encounter a lost update because it does not block the browser drawing.
+   * It is preferred to use it in a separate `useLayoutEffect`, and the multiple calls within it through `requestAnimationFrame` 
+   * will be merged and submitted for update in the same frame after the react rendering component is completed.
+   */
   update() {
-    console.log('update call')
-    if (this.updateTime > 0) {
-      clearTimeout(this.updateTimer)
-      this.updateTimer = setTimeout(() => {
-        this.styleElement.textContent = this.cssLayer.valueOf()
-        console.log('update cssVar')
-      }, this.updateTime)
-    } else {
-      this.styleElement.textContent = this.cssLayer.valueOf()
-    }
+    //合并更新任务于下一次重绘前
+    if (this.req) return
+    this.req = true
+
+    requestAnimationFrame(() => {
+      this.styleElement.innerHTML = this.cssLayer.valueOf()
+      this.req = false
+    })
   }
 }
